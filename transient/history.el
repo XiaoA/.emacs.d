98cce15c75f723264f74107b15885d7aa7236c73
Andrew Buckingham <abuckingham99@gmail.com>
2022-10-20 19:39:26 -0700

    update config

--- transient/history.el
+++ transient/history.el
@@ -1,4 +1,6 @@
-((magit-branch nil)
+((magit-blame
+  ("-w"))
+ (magit-branch nil)
  (magit-commit nil)
  (magit-diff
   ("--no-ext-diff" "--stat")
@@ -6,19 +8,30 @@
    "--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-fetch nil)
+ (magit-file-dispatch nil)
  (magit-gitignore nil)
  (magit-log
+  ("-n256" "--graph" "--decorate")
+  (("--" "./"))
+  (("--" "./")
+   "--graph")
+  ("-n256" "--graph" "--color" "--decorate")
   ("--graph" "--decorate")
   ("-n256"
    ("--" "--")
    "--graph" "--color" "--decorate")
   ("--graph" "--color"))
- (magit-merge nil)
+ (magit-merge nil
+              ("--ff-only"))
  (magit-pull nil)
- (magit-push nil)
+ (magit-push nil
+             ("--force"))
  (magit-rebase nil)
  (magit-remote
   ("-f"))
  (magit-reset nil)
+ (magit-revert
+  ("--edit"))
  (magit-stash nil)
+ (magit-status-jump nil)
  (magit:-- "--" "--color" ""))
