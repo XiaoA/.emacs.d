662978b596659d731cff4d1ff06d5938281d1148
Andrew Buckingham <abuckingham99@gmail.com>
2025-11-23 19:42:31 -0800

    Remove old config blocks

--- abuckingham99.el
+++ abuckingham99.el
@@ -410,19 +410,6 @@
 (add-hook 'ruby-mode-hook 'minitest-mode)
 (add-hook 'enh-ruby-mode-hook 'minitest-mode)
 
-;;  (add-to-list 'load-path "~/.emacs.d/slime")
-;;  (setq inferior-lisp-program "/usr/local/bin/sbcl")
-;;  (require 'slime)
-;;  (slime-setup)
-
-;  (autoload 'js2-mode "js2-mode" nil t)
-;  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-;  (global-set-key [f5] 'slime-js-reload)
-;  (add-hook 'js2-mode-hook
-;            (lambda ()
-;              (slime-js-minor-mode 1)))
-; (load-file "~/.emacs.d/setup-slime-js.el")
-
 ;; (require 'ob-elixir)
 
 (use-package exec-path-from-shell
