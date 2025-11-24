3d0b9c3556c628c7ad11b73e90559bd4bca03ca9
Andrew Buckingham <abuckingham99@gmail.com>
2025-11-23 18:25:53 -0800

    Update yas-snippet/popup config

--- abuckingham99.el
+++ abuckingham99.el
@@ -132,51 +132,6 @@
   :config
   (import-env-from-shell-initialize))
 
-(use-package yasnippet
-  :config
-
-  (setq yas-snippet-dirs
-        '("~/.emacs.d/snippets"
-          "~/.emacs.d/snippets/web-mode"
-          "~/.emacs.d/snippets/markdown-mode"
-          "~/.emacs.d/snippets/org-mode"
-          "~/.emacs.d/snippets/ruby-mode"))
-  (yas-global-mode 1))
-
-;;Load Popup-Snippets
-(add-to-list 'load-path "~/.emacs.d/vendor/")
-
-(require 'popup)
-;; add some shotcuts in popup menu mode
-(define-key popup-menu-keymap (kbd "M-n") 'popup-next)
-( define-key popup-menu-keymap (kbd "TAB") 'popup-next)
-(define-key popup-menu-keymap (kbd "<tab>") 'popup-next)
-(define-key popup-menu-keymap (kbd "<backtab>") 'popup-previous)
-(define-key popup-menu-keymap (kbd "M-p") 'popup-previous)
-
-(defun yas/popup-isearch-prompt (prompt choices &optional display-fn)
-  (when (featurep 'popup)
-    (popup-menu*
-     (mapcar
-      (lambda (choice)
-        (popup-make-item
-         (or (and display-fn (funcall display-fn choice))
-             choice)
-         :value choice))
-      choices)
-     :prompt prompt
-     ;; start isearch mode immediately
-     :isearch t
-     )))
-
-(setq yas/prompt-functions '(yas/popup-isearch-prompt yas/no-prompt))
-
-(setq yas-snippet-dirs
-      '("~/.emacs.d/elpa/yasnippet-20140314.255/snippets/"
-        "~/.emacs.d/snippets/"
-        ))
-(yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
-
 ;; From: https://github.com/fxbois/web-mode/issues/51
 ;; Fixes Yassnippet with web-mode
 
